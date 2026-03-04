806d0c9cc1a66226142482f71c7e71f06833eab3
prateek180601 <pratsovatbusiness@gmail.com>
2026-03-04 22:22:18 +0530

    Implement Razorpay payment button
    Added payment button functionality using Razorpay.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+document.getElementById('payBtn').onclick = function () {
+    var options = {
+        "key": "YOUR_RAZORPAY_KEY",
+        "amount": "50000",
+        "currency": "INR",
+        "name": "Udaan Care",
+        "description": "Appointment Booking",
+        "handler": function (response){
+            alert("Payment Successful!");
+            window.location.href = "thankyou.html";
+        }
+    };
+    var rzp = new Razorpay(options);
+    rzp.open();
+};
